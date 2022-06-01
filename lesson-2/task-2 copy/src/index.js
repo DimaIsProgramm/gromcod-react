8e399aafa09f6bd1fbdd33e87d9fd7aa26755f18
DimaIsProgramm <93841542+DimaIsProgramm@users.noreply.github.com>
2022-06-01 14:50:02 +0300

    Add files via upload

--- lesson-2/task-2 copy/src/index.js
+++ lesson-2/task-2 copy/src/index.js
@@ -4,22 +4,17 @@ import './styles.css';
 
 const rootElement = document.querySelector('#root');
 
-const renderSeconds = time => {
-  const seconds = new Date(time).getSeconds();
-  const backgroundColor = seconds % 2 === 0 ? '#fff' : '#000';
-  const textColor = seconds % 2 === 0 ? '#000' : '#fff';
-  const styles = {
-    color: textColor,
-    backgroundColor: backgroundColor,
-  };
+const element = (
+  <>
+    <h1 className="title">Todo List</h1>
+    <main className="todo-list">
+      <div className="actions">
+        <input className="task-input" type="text" />
+        <button className="btn create-task-btn">Create</button>
+      </div>
+      <ul className="list"></ul>
+    </main>
+  </>
+);
 
-  const element = (
-    <div className="seconds" style={styles}>
-      {seconds}
-    </div>
-  );
-
-  ReactDOM.render(element, rootElement);
-};
-
-setInterval(() => renderSeconds(new Date()), 1000);
+ReactDOM.render(element, rootElement);
