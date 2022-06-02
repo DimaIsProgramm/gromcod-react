b5f904707a0b8dc129627794f270e2effd06d56d
DimaIsProgramm <93841542+DimaIsProgramm@users.noreply.github.com>
2022-06-02 12:50:06 +0300

    Add files via upload

--- lesson-3/task-1/src/Search.jsx
+++ lesson-3/task-1/src/Search.jsx
@@ -1,21 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.scss';
-import './search.scss';
 
-const rootElement = document.querySelector('#root');
-
-export default Search = props => {
+export default function (props) {
   const element = (
     <div className="search">
       <h1 className="search__title">{`Hello, ${props.name}. What to search for you?`}</h1>
       <div className="search__field">
-        <input type="text" class="search__input" />
+        <input type="text" className="search__input" />
         <button className="search__button">Search</button>
       </div>
     </div>
   );
   return element;
-};
-
-ReactDOM.render(<Search name="Tom" />, rootElement);
+}
