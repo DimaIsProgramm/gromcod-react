45483b275452db1497912921336c77c0c489ebe5
DimaIsProgramm <93841542+DimaIsProgramm@users.noreply.github.com>
2022-06-12 14:59:19 +0300

    Update UserInfo.jsx

--- lesson-3/task-3/src/UserInfo.jsx
+++ lesson-3/task-3/src/UserInfo.jsx
@@ -5,7 +5,7 @@ const UserInfo = props => {
   return (
     <div className="user-info">
       <UserAvatar user={props.user} />
-      <div className="user-info__name">{props.user.name}</div>
+      <div className="user-info__name">{props.user}</div>
     </div>
   );
 };
