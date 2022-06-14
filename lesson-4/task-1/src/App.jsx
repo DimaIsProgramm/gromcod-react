98700dd46ebcbe8ef2aedb79d611b49c7b0978fb
DimaIsProgramm <93841542+DimaIsProgramm@users.noreply.github.com>
2022-06-14 18:09:28 +0300

    Add files via upload

---
+++ lesson-4/task-1/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Counter from './Counter';
+
+const App = () => {
+  return (
+    <>
+      <Counter start={5} interval={1000} />
+      <Counter start={12} interval={500} />
+      <Counter start={-21} interval={1500} />
+    </>
+  );
+};
+
+export default App;
