5488883e6eb15a3b20d57fd7f27606605d892dc3
DimaIsProgramm <93841542+DimaIsProgramm@users.noreply.github.com>
2022-06-14 16:55:11 +0300

    Update Profile.jsx

--- lesson-3/task-5/src/Profile.jsx
+++ lesson-3/task-5/src/Profile.jsx
@@ -5,7 +5,7 @@ const Profile = ({ userData }) => {
   return (
     <div className="greeting">
       <span className="profile__name">
-        {userData.firstName} {userData.lastName}.
+        {userData.firstName} {userData.lastName}
       </span>
       <span className="profile__birth">
         Was born {moment(userData.birthDate).format('DD MMM YY')} in {userData.birthPlace}
