64349d3c28293bcacc0825aaf383ea73aab17887
DimaIsProgramm <93841542+DimaIsProgramm@users.noreply.github.com>
2022-06-16 13:17:56 +0300

    Update ColorPicker.jsx

--- lesson-5/task-6/src/ColorPicker.jsx
+++ lesson-5/task-6/src/ColorPicker.jsx
@@ -1,9 +1,5 @@
 import React, { Component } from 'react';
 
-const CORAL = 'coral';
-const AQUA = 'aqua';
-const BISQUE = 'bisque';
-
 class ColorPicker extends Component {
   constructor(props) {
     super(props);
@@ -52,19 +48,16 @@ class ColorPicker extends Component {
         <div>
           <button
             class="picker__button picker__button_coral"
-            style={{ backgroundColor: CORAL }}
             onMouseEnter={this.onEnterCoral}
             onMouseLeave={this.outLeaveCoral}
           ></button>
           <button
             class="picker__button picker__button_aqua"
-            style={{ backgroundColor: AQUA }}
             onMouseEnter={this.onEnterAqua}
             onMouseLeave={this.outLeaveAqua}
           ></button>
           <button
             class="picker__button picker__button_bisque"
-            style={{ backgroundColor: BISQUE }}
             onMouseEnter={this.onEnterBisque}
             onMouseLeave={this.outLeaveBisque}
           ></button>
