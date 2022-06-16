4afa1de6c0d149b65115e8425f426ae174f5c282
DimaIsProgramm <93841542+DimaIsProgramm@users.noreply.github.com>
2022-06-16 19:45:51 +0300

    Add files via upload

--- lesson-6/task-3/src/index.jsx
+++ lesson-6/task-3/src/index.jsx
@@ -5,4 +5,4 @@ import './index.scss';
 
 const rootElement = document.querySelector('#root');
 
-ReactDOM.render(<Mailbox unreadMessages={[]} />, rootElement);
+ReactDOM.render(<Mailbox unreadMessages={['a', 'b']} />, rootElement);
