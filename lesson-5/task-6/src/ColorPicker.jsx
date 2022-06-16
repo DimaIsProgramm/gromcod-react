6e0457f6c459eb073a402fc006d024f257f99c5c
DimaIsProgramm <93841542+DimaIsProgramm@users.noreply.github.com>
2022-06-16 11:27:42 +0300

    Add files via upload

--- lesson-5/task-6/src/ColorPicker.jsx
+++ lesson-5/task-6/src/ColorPicker.jsx
@@ -1,35 +1,82 @@
 import React, { Component } from 'react';
 
-const RED = '#f00';
-const GREEN = '#0f0';
-const BLUE = '#00f';
+const CORAL = 'coral';
+const AQUA = 'aqua';
+const BISQUE = 'bisque';
 
-class Colors extends Component {
-  setBodyColor = color => {
-    document.body.style.backgroundColor = color;
+class ColorPicker extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: '',
+    };
+  }
+
+  onClickCoral = () => {
+    this.setState({
+      text: 'Coral',
+    });
+  };
+  outClickCoral = () => {
+    this.setState({
+      text: '',
+    });
+  };
+
+  onClickAqua = () => {
+    this.setState({
+      text: 'Aqua',
+    });
+  };
+  outClickAqua = () => {
+    this.setState({
+      text: '',
+    });
+  };
+
+  onClickBisque = () => {
+    this.setState({
+      text: 'Bisque',
+    });
+  };
+  outClickBisque = () => {
+    this.setState({
+      text: '',
+    });
   };
 
   render() {
     return (
-      <div class="colors">
-        <button
-          style={{ backgroundColor: RED }}
-          className="colors__button"
-          onClick={() => this.setBodyColor(RED)}
-        ></button>
-        <button
-          style={{ backgroundColor: GREEN }}
-          className="colors__button"
-          onClick={() => this.setBodyColor(GREEN)}
-        ></button>
-        <button
-          style={{ backgroundColor: BLUE }}
-          className="colors__button"
-          onClick={() => this.setBodyColor(BLUE)}
-        ></button>
+      <div>
+        <div class="picker__title">{this.state.text}</div>
+        <div>
+          <button
+            class="picker__button picker__button_coral"
+            style={{ backgroundColor: CORAL }}
+            onMouseEnter={this.onClickCoral}
+            onMouseLeave={this.outClickCoral}
+          ></button>
+          <button
+            class="picker__button picker__button_aqua"
+            style={{ backgroundColor: AQUA }}
+            onMouseEnter={this.onClickAqua}
+            onMouseLeave={this.outClickAqua}
+          ></button>
+          <button
+            class="picker__button picker__button_bisque"
+            style={{ backgroundColor: BISQUE }}
+            onMouseEnter={this.onClickBisque}
+            onMouseLeave={this.outClickBisque}
+          ></button>
+        </div>
       </div>
     );
   }
 }
 
-export default Colors;
+export default ColorPicker;
+
+// algo
+// навесить 2 оюработчика на кнопку
+// при попадании курсора в квадрат показівать текст
+// при покидании кнопки очищать текст
