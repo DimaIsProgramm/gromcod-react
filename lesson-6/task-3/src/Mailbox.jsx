c357574dd93ca88601543f784d69d84993d4097e
DimaIsProgramm <93841542+DimaIsProgramm@users.noreply.github.com>
2022-06-16 19:43:56 +0300

    Update Mailbox.jsx

--- lesson-6/task-3/src/Mailbox.jsx
+++ lesson-6/task-3/src/Mailbox.jsx
@@ -4,7 +4,7 @@ import './index.scss';
 const Mailbox = ({ unreadMessages }) => {
   return (
     <div className="mailbox">
-      <span className="mailbox__text">Message</span>
+      <span className="mailbox__text">Messages</span>
       {unreadMessages.length > 0 && <span className="mailbox__count">{unreadMessages.length}</span>}
     </div>
   );
