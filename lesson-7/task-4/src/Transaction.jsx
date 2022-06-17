56452604912ad2283171909d4f0063c5aca04e8f
DimaIsProgramm <93841542+DimaIsProgramm@users.noreply.github.com>
2022-06-17 14:03:01 +0300

    Update Transaction.jsx

--- lesson-7/task-4/src/Transaction.jsx
+++ lesson-7/task-4/src/Transaction.jsx
@@ -8,7 +8,7 @@ const Transaction = ({ from, to, amount, rate, time }) => {
       <span className="transaction__time">{moment(time).format('HH' + ':' + 'mm')}</span>
       <span className="transaction__assets">{`${from} - ${to}`}</span>
       <span className="transaction__rate">{new Intl.NumberFormat('en-GB').format(rate)}</span>
-      <span className="transaction__amount">{amount}</span>
+      <span className="transaction__amount">{new Intl.NumberFormat('en-GB').format(amount)}</span>
     </li>
   );
 };
