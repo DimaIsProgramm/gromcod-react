2a8d0a1ef30676f5cda494ecaa07e828bd027aab
DimaIsProgramm <93841542+DimaIsProgramm@users.noreply.github.com>
2022-06-26 12:41:16 +0300

    Add files via upload

--- lesson-7/task-1/src/Pagination.jsx
+++ lesson-7/task-1/src/Pagination.jsx
@@ -1,26 +1,25 @@
 import React from 'react';
 
 const Pagination = ({ goPrev, goNext, currentPage, totalItems, itemsPerPage }) => {
-  const totalStr = Math.ceil(totalItems / itemsPerPage);
-  const isPrevPageAvailable = currentPage === 1;
-  const isNextPageAvailable = currentPage === totalStr;
+  const isPrevPageAvailable = currentPage > 1;
+  const isNextPageAvailable = totalItems - itemsPerPage * currentPage > 0;
   return (
     <div className="pagination">
       {isPrevPageAvailable ? (
-        <button className="btn" onClick={goPrev} disabled></button>
-      ) : (
         <button className="btn" onClick={goPrev}>
           ←
         </button>
+      ) : (
+        <button className="btn" onClick={goPrev} disabled></button>
       )}
 
       <span className="pagination__page">{currentPage}</span>
       {isNextPageAvailable ? (
-        <button className="btn" onClick={goNext} disabled></button>
-      ) : (
         <button className="btn" onClick={goNext}>
           →
         </button>
+      ) : (
+        <button className="btn" onClick={goNext} disabled></button>
       )}
     </div>
   );
