33040e7abb64516cdc5708ba7ae1195bfe27b336
DimaIsProgramm <93841542+DimaIsProgramm@users.noreply.github.com>
2022-06-27 11:53:57 +0300

    Add files via upload

--- lesson-9/task-2/src/App.jsx
+++ lesson-9/task-2/src/App.jsx
@@ -3,10 +3,12 @@ import UserForm from './UserForm';
 
 class App extends React.Component {
   createUser(user) {
+    event.preventDefault();
     console.log(user);
   }
+
   render() {
-    return <UserForm onSubmit={this.createUser(this.props.user)} />;
+    return <UserForm onSubmit={this.createUser} />;
   }
 }
 
