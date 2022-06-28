2ebb393683691fb985bc1bc63c1e90a68e89e7f9
DimaIsProgramm <93841542+DimaIsProgramm@users.noreply.github.com>
2022-06-28 13:53:08 +0300

    Add files via upload

---
+++ lesson-11/task-2/src/App.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Dialog from './Dialog';
+
+class App extends React.Component {
+  state = {
+    isOpen: false,
+  };
+  hideDialog = () => {
+    this.setState({
+      isOpen: false,
+    });
+  };
+  showDialog = () => {
+    this.setState({
+      isOpen: true,
+    });
+  };
+
+  render() {
+    return (
+      <div className="app">
+        <button className="btn" onClick={this.showDialog}>
+          Show
+        </button>
+        <Dialog isOpen={this.state.isOpen} onClose={this.hideDialog} title="Recomendation">
+          <p>Use immutable array methods to work with data. It will help to avoid bugs</p>
+        </Dialog>
+      </div>
+    );
+  }
+}
+
+export default App;
