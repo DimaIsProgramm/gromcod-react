0161cd8144956aac5c86f5f428d7f8a9d26947b6
DimaIsProgramm <93841542+DimaIsProgramm@users.noreply.github.com>
2022-06-28 16:53:52 +0300

    Add files via upload

--- lesson-11/task-3/src/App.jsx
+++ lesson-11/task-3/src/App.jsx
@@ -1,41 +1,17 @@
 import React from 'react';
 import Expand from './Expand';
 
-class App extends React.Component {
-  state = {
-    isOpen: false,
-  };
-
-  hideContent = () => {
-    this.setState({
-      isOpen: false,
-    });
-  };
-  showContent = () => {
-    this.setState({
-      isOpen: true,
-    });
-  };
-
-  render() {
-    const { isOpen } = this.state;
-
-    return (
-      <div className="app">
-        <Expand
-          title="Awsome title"
-          onClose={this.hideContent}
-          isOpen={isOpen}
-          onOpen={this.showContent}
-        >
-          <p>
-            Hooks are a new addition in React 16.8. They let you use state and other React features
-            without writing a class.
-          </p>
-        </Expand>
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div className="app">
+      <Expand title="Awsome title">
+        <p>
+          Hooks are a new addition in React 16.8. They let you use state and other React features
+          without writing a class.
+        </p>
+      </Expand>
+    </div>
+  );
+};
 
 export default App;
