9fd761d73e37cb60758a76c2d7e267352d271229
DimaIsProgramm <93841542+DimaIsProgramm@users.noreply.github.com>
2022-06-28 09:18:34 +0300

    Add files via upload

---
+++ lesson-10/task-3/src/TemperatureInput.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const scaleNames = {
+  c: 'Celsius',
+  f: 'Fahrenheit',
+};
+
+class TemperatureInput extends React.Component {
+  handleChange = event => {
+    this.props.onTemperatureChange(event.target.value);
+  };
+  render() {
+    const temperature = this.props.temperature;
+    const scale = this.props.scale;
+    return (
+      <fieldset>
+        <legend>Enter temperature is {scaleNames[scale]}:</legend>
+        <input value={temperature} onChange={this.handleChange} />
+      </fieldset>
+    );
+  }
+}
+
+export default TemperatureInput;
