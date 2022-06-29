21720def628da66e9990d0b6e5c9b7b1dc6f06fc
DimaIsProgramm <93841542+DimaIsProgramm@users.noreply.github.com>
2022-06-29 20:20:20 +0300

    Add files via upload

---
+++ lesson-14/task-3/src/Dimensions.jsx
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from 'react';
+
+const Dimensions = () => {
+  const [demensions, setDimensions] = useState({ width: null, height: null });
+
+  useEffect(() => {
+    const { innerWidth, innerHeight } = window;
+    setDimensions({ width: innerWidth, height: innerHeight });
+    const onResize = e => {
+      const { innerWidth, innerHeight } = e.target;
+      setDimensions({ width: innerWidth, height: innerHeight });
+    };
+    window.addEventListener('resize', onResize);
+
+    return () => {
+      window.removeEventListener('resize', onResize);
+    };
+  }, []);
+
+  const { width, height } = demensions;
+  return <div className="dimensions">{`${width}px - ${height}px`}</div>;
+};
+
+export default Dimensions;
