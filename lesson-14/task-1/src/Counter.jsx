d5e3c3d8219651ec7cd2503bfd53a328fa7fe46a
DimaIsProgramm <93841542+DimaIsProgramm@users.noreply.github.com>
2022-06-29 18:53:52 +0300

    Add files via upload

--- lesson-14/task-1/src/Counter.jsx
+++ lesson-14/task-1/src/Counter.jsx
@@ -1,48 +1,29 @@
-import React from 'react';
-import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
+import React, { useState } from 'react';
 
-class Counter extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      counter: 0,
-    };
-    this.decrement = this.decrement.bind(this);
-  }
+const Counter = () => {
+  const [counter, setCounter] = useState(0);
 
-  decrement() {
-    this.setState({
-      counter: this.state.counter - 1,
-    });
-  }
-
-  increment = () => {
-    this.setState({
-      counter: this.state.counter + 1,
-    });
-  };
-
-  reset() {
-    this.setState({
-      counter: 0,
-    });
-  }
-
-  render() {
-    return (
-      <div class="counter">
-        <button data-action="decrease" className="counter__button" onClick={this.decrement}>
-          -
-        </button>
-        <span className="counter__value" onClick={() => this.reset()}>
-          {this.state.counter}
-        </span>
-        <button data-action="increase" className="counter__button" onClick={this.increment}>
-          +
-        </button>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="counter">
+      <button
+        data-action="decrease"
+        className="counter__button"
+        onClick={() => setCounter(counter - 1)}
+      >
+        -
+      </button>
+      <span className="counter__value" onClick={() => setCounter(0)}>
+        {counter}
+      </span>
+      <button
+        data-action="increase"
+        className="counter__button"
+        onClick={() => setCounter(counter + 1)}
+      >
+        +
+      </button>
+    </div>
+  );
+};
 
 export default Counter;
